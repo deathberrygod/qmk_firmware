7a83a90f6d41a0f384db6591255be788c0fc0d4d
Daniel G <danimentos@hotmail.com>
2025-06-04 21:15:40 -0500

    Update keymap.c

--- keyboards/keebio/iris_lm/keymaps/deathberryv1/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/deathberryv1/keymap.c
@@ -3,6 +3,20 @@
 
 #include QMK_KEYBOARD_H
 
+typedef union {
+  uint32_t raw;
+  struct {
+    bool     rgb_layer_change :1;
+  };
+} user_config_t;
+
+user_config_t user_config;
+
+enum my_keycodes {
+  FOO = SAFE_RANGE,
+  RGB_LYR
+};
+
 enum custom_layers {
 //   _COLEMAK = 0,
 	 _QWERTY = 0,
@@ -15,6 +29,7 @@ enum custom_layers {
 // #define KC_COLM DF(_COLEMAK)
 
 #define KC_CAD LCTL(LALT(KC_DEL))
+#define KC_CSE LCTL(LSFT(KC_ENT))
 #define RB_TOG RGB_TOG
 #define RB_MOD RGB_MOD
 
@@ -49,44 +64,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ┌──────┬──────┬──────┬──────┬──────┬──────┐                   ┌──────┬──────┬──────┬──────┬──────┬──────┐
  * │ ESC  │   1  │   2  │   3  │   4  │   5  │                   │   6  │   7  │   8  │   9  │   0  │ Bspc │
  * ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Tab  │   Q  │   W  │   E  │   R  │   T  │                   │   Y  │   U  │   I  │   O  │   P  │  - _ │
+ * │ Tab  │   Q  │   W  │   E  │   R  │   T  │                   │   Y  │   U  │   I  │   O  │   P  │ Enter│
  * ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
  * │LShift│   A  │   S  │   D  │   F  │   G  │                   │   H  │   J  │   K  │   L  │  ' " │RShift│
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┐     ┌──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ LCTR │   Z  │   X  │   C  │   V  │   B  │ RGui │     │Enter │   N  │   M  │  , < │  . > │  / ? │  \ | │
+ * │ LCTR │   Z  │   X  │   C  │   V  │   B  │  / ? │     │  \ | │   N  │   M  │  , < │  . > │  - _ │ RGui │
  * └──────┴──────┴──────┴────┬─┴────┬─┴────┬─┴────┬─┘     └─┬────┴─┬────┴─┬────┴─┬────┴──────┴──────┴──────┘
- *                           │ LOWER│ LAlt │ Caps │         │Space │ RAlt │UPPER │
+ *                           │ LOWER│ LAlt │Space │         │Space │ RAlt │UPPER │
  *                           └──────┴──────┴──────┘         └──────┴──────┴──────┘
  */
   
   [_QWERTY] = LAYOUT(
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
      KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_RSFT,
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LGUI,        KC_ENT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                    TL_LOWR, KC_LALT, KC_CAPS,                KC_SPC,  KC_RALT,  TL_UPPR
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_SLSH,       KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, KC_RGUI,
+                                    TL_LOWR, KC_LALT, KC_SPC,                KC_SPC,  KC_RALT,  TL_UPPR
   ),
 
 /* LOWER - Lower layer
  * ┌──────┬──────┬──────┬──────┬──────┬──────┐                   ┌──────┬──────┬──────┬──────┬──────┬──────┐
  * │      │      │      │      │      │      │                   │      │      │      │      │      │ C+A+D│
  * ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
- * │      │      │      │      │   {  │   }  │                   │      │      │   Up │      │      │   +  │
+ * │      │      │      │      │   {  │   }  │                   │      │   +  │   Up │   =  │      │ C+S+E│
  * ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
- * │      │      │      │      │   [  │   ]  │                   │      │ Left │ Down │ Right│      │   =  │
+ * │      │      │      │      │   [  │   ]  │                   │      │ Left │ Down │ Right│      │      │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┐     ┌──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │      │      │      │      │      │      │      │     │      │      │      │   ;  │   :  │      │      │
  * └──────┴──────┴──────┴────┬─┴────┬─┴────┬─┴────┬─┘     └─┬────┴─┬────┴─┬────┴─┬────┴──────┴──────┴──────┘
- *                           │      │      │      │         │      │      │      │
+ *                           │      │      │      │         │ Caps │      │      │
  *                           └──────┴──────┴──────┘         └──────┴──────┴──────┘
  */
 
   [_LOWER] = LAYOUT(
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, KC_CAD,
-     _______, _______, _______, _______, KC_LCBR, KC_RCBR,                            _______, _______, KC_UP,   _______, _______, KC_PLUS,
-     _______, _______, _______, _______, KC_LBRC, KC_RBRC,                            _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_EQL,
+     _______, _______, _______, _______, KC_LCBR, KC_RCBR,                            _______, KC_PLUS, KC_UP,   KC_EQL,  _______, KC_CSE,
+     _______, _______, _______, _______, KC_LBRC, KC_RBRC,                            _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,
      _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, KC_SCLN, KC_COLN, _______, _______,
-                                    _______, _______, _______,                    _______, _______, _______
+                                    _______, _______, _______,                    KC_CAPS, _______, _______
   ),
 
 /* UPPER - Upper layer
@@ -97,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
  * │      │      │      │      │      │      │                   │      │      │      │      │      │      │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┐     ┌──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ RGBT │ RGBM │      │      │      │ PTRS │      │     │      │      │      │      │      │      │      │
+ * │ RGBT │ RGBM │  LYR │      │      │ PTRS │      │     │      │      │      │      │      │      │      │
  * └──────┴──────┴──────┴────┬─┴────┬─┴────┬─┴────┬─┘     └─┬────┴─┬────┴─┬────┴─┬────┴──────┴──────┴──────┘
  *                           │      │      │      │         │      │      │      │
  *                           └──────┴──────┴──────┘         └──────┴──────┴──────┘
@@ -107,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              _______, _______, _______, _______, _______, _______,
      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                             _______, _______, _______, _______, _______, _______,
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-     RB_TOG,  RB_MOD,  _______, _______, _______, KC_PSCR, _______,          _______, _______, _______, _______, _______, _______, _______,
+     RB_TOG,  RB_MOD,  RGB_LYR, _______, _______, KC_PSCR, _______,          _______, _______, _______, _______, _______, _______, _______,
                                     _______, _______, _______,                   _______, _______, _______
   ),
 
@@ -117,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
  * │      │      │      │      │      │ Boot │                   │ Boot │      │      │      │      │      │
  * ├──────┼──────┼──────┼──────┼──────┼──────┤                   ├──────┼──────┼──────┼──────┼──────┼──────┤
- * │      │      │      │      │      │      │                   │      │      │      │      │      │      │
+ * │      │      │      │      │      │EEPROM│                   │      │      │      │      │      │      │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┐     ┌──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │      │      │      │      │      │      │      │     │      │      │      │      │      │      │      │
  * └──────┴──────┴──────┴────┬─┴────┬─┴────┬─┴────┬─┘     └─┬────┴─┬────┴─┬────┴─┬────┴──────┴──────┴──────┘
@@ -128,100 +143,77 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT(
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
      _______, _______, _______, _______, _______, QK_BOOT,                            QK_BOOT, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, EE_CLR,                             _______, _______, _______, _______, _______, _______,
      _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
                                     _______, _______, _______,                   _______, _______, _______
   )
   
 };
 
-/* bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    for (uint8_t i = led_min; i < led_max; i++) {
-        switch(get_highest_layer(layer_state|default_layer_state)) {
-			case _ADJUST:
-				rgb_matrix_enable_noeeprom()
-                rgb_matrix_mode_noeeprom(RGB_MATRIX_EFFECT_SOLID_COLOR);
-				rgb_matrix_sethsv_noeeprom(HSV_WHITE); // white
-                break;
-			case _UPPER:
-				rgb_matrix_enable_noeeprom()
-				rgb_matrix_mode_noeeprom(RGB_MATRIX_EFFECT_SOLID_COLOR);
-				rgb_matrix_sethsv_noeeprom(HSV_GOLD); // gold
-                break;
-			case _LOWER:
-				rgb_matrix_enable_noeeprom()
-                rgb_matrix_mode_noeeprom(RGB_MATRIX_EFFECT_SOLID_COLOR);
-				rgb_matrix_sethsv_noeeprom(HSV_BLUE); // blue
-                break;
-            case _QWERTY:
-				rgb_matrix_enable_noeeprom()
-                rgb_matrix_mode_noeeprom(RGB_MATRIX_EFFECT_SOLID_COLOR);
-				rgb_matrix_sethsv_noeeprom(HSV_MAGENTA); // magenta
-                break;
-            default:
-				rgb_matrix_disable_noeeprom();
-                break;
-        }
-    }
-    return false;
+void keyboard_post_init_user(void) {
+  // Call the keymap level matrix init.
+
+  // Read the user config from EEPROM
+  user_config.raw = eeconfig_read_user();
+
+  // Set default layer, if enabled
+  if (user_config.rgb_layer_change) {
+    rgb_matrix_enable_noeeprom();
+    rgb_matrix_sethsv_noeeprom(HSV_GOLD);
+    rgb_matrix_mode_noeeprom(1);
+  }
 }
 
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (get_highest_layer(layer_state) > 0) {
-        uint8_t layer = get_highest_layer(layer_state);
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case _ADJUST:
+        if (user_config.rgb_layer_change) { rgb_matrix_sethsv_noeeprom(HSV_WHITE); rgb_matrix_mode_noeeprom(1); }
+        break;
+    case _UPPER:
+        if (user_config.rgb_layer_change) { rgb_matrix_sethsv_noeeprom(HSV_SPRINGGREEN); rgb_matrix_mode_noeeprom(1); }
+        break;
+    case _LOWER:
+        if (user_config.rgb_layer_change) { rgb_matrix_sethsv_noeeprom(HSV_PURPLE); rgb_matrix_mode_noeeprom(1); }
+        break;
+    default: //  for any other layers, or the default layer
+        if (user_config.rgb_layer_change) { rgb_matrix_sethsv_noeeprom(HSV_GOLD); rgb_matrix_mode_noeeprom(1); }
+        break;
+    }
+  return state;
+}
 
-        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-                uint8_t index = g_led_config.matrix_co[row][col];
-
-                if (index >= led_min && index < led_max && index != NO_LED &&
-                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_GREEN);
-                }
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RGB_LYR:  // This allows me to use RGB as layer indication, or as normal
+        if (record->event.pressed) {
+            user_config.rgb_layer_change ^= 1; // Toggles the status
+            eeconfig_update_user(user_config.raw); // Writes the new status to EEPROM
+            if (user_config.rgb_layer_change) { // if layer state indication is enabled,
+                layer_state_set(layer_state);   // then immediately update the layer color
             }
         }
-    }
-    return false;
-}
-*/
-
-/*
-
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (get_highest_layer(layer_state) > 0) {
-        uint8_t layer = get_highest_layer(layer_state); 
-
-        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-                uint8_t led_index = g_led_config.matrix_co[row][col]; // Get the LED index for the key position
-
-                if (index >= led_min && index < led_max && index != NO_LED &&
-                    keymap_key_to_keycode(layer, (keypos_t){col, row}) > KC_TRNS) {
-
-                    switch (layer) {
-                        case _ADJUST: // Or use your layer name, e.g., _ADJUST
-                            rgb_matrix_mode_noeeprom(RGB_MATRIX_EFFECT_SOLID_COLOR);
-							rgb_matrix_sethsv_noeeprom(HSV_WHITE); // WHITE
-                            break;
-
-                        case _UPPER: // Or use your layer name, e.g., _UPPER
-                            rgb_matrix_mode_noeeprom(RGB_MATRIX_EFFECT_SOLID_COLOR);
-							rgb_matrix_sethsv_noeeprom(HSV_GOLD); // GOLD
-                            break;
-
-                        case _LOWER: // Or use your layer name, e.g., _LOWER
-                            rgb_matrix_mode_noeeprom(RGB_MATRIX_EFFECT_SOLID_COLOR);
-							rgb_matrix_sethsv_noeeprom(HSV_MAGENTA); // MAGENTA
-                            break;
-
-                        default:
-                            break;
-                    }
-                }
+        return false;
+    case RGB_MODE_FORWARD ... RGB_MODE_GRADIENT: // For any of the RGB codes (see quantum_keycodes.h, L400 for reference)
+        if (record->event.pressed) { //This disables layer indication, as it's assumed that if you're changing this ... you want that disabled
+            if (user_config.rgb_layer_change) {        // only if this is enabled
+                user_config.rgb_layer_change = false;  // disable it, and
+                eeconfig_update_user(user_config.raw); // write the setings to EEPROM
             }
         }
-    }
-    return false;
+        return true; break;
+    default:
+      return true; // Process all other keycodes normally
+  }
 }
 
-*/
+void eeconfig_init_user(void) {  // EEPROM is getting reset!
+  user_config.raw = 0;
+  user_config.rgb_layer_change = true; // We want this enabled by default
+  eeconfig_update_user(user_config.raw); // Write default value to EEPROM now
+
+  // use the non noeeprom versions, to write these values to EEPROM too
+  rgb_matrix_enable(); // Enable RGB by default
+  rgb_matrix_sethsv(HSV_GOLD);  // Set it to GOLD by default
+  rgb_matrix_mode(1); // set to solid by default
+}
+
